d911e16e3071953f45ec5036308359888f4b3b1b
Manfrinmm <matheus_mm123@hotmail.com>
2019-07-28 01:16:43 -0300

    fix building

--- android/app/src/main/java/com/modulo2/MainApplication.java
+++ android/app/src/main/java/com/modulo2/MainApplication.java
@@ -31,10 +31,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages(),
+      List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
-      new CodePush(BuildConfig.CODEPUSH_KEY,getApplicationContext(),BuildConfig.DEBUG);
+      // packages.add( new CodePush(BuildConfig.CODEPUSH_KEY,getApplicationContext(),BuildConfig.DEBUG));
       return packages;
     }
 

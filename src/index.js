3ac46525b517a4789e867fbdfba8416fbeb69096
Manfrinmm <matheus_mm123@hotmail.com>
2019-07-28 00:09:57 -0300

    add code notification

--- src/index.js
+++ src/index.js
@@ -5,7 +5,36 @@ import { AsyncStorage } from "react-native";
 
 import createNavigator from "./routes";
 
-export default class App extends Component {
+import CodePush from "react-native-code-push";
+import OneSignal from "react-native-onesignal";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init("b6dc5aee-a10e-4900-b490-991bed198d36");
+    OneSignal.addEventListener("received", this.onReceive);
+    OneSignal.addEventListener("opened", this.onOpened);
+    OneSignal.addEventListener("ids", this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener("received", this.onReceive);
+    OneSignal.removeEventListener("opened", this.onOpened);
+    OneSignal.removeEventListener("ids", this.onIds);
+  }
+
+  //usuario recebe uma notifcação quando a aplicação estiver aberta
+  onReceive = data => {
+    console.tron.log(data);
+  };
+
+  //quando clica na notificação com app fechado
+  onOpened = notification => {};
+
+  //quando o usuario faz um registro  no serviço de notificação, geralmente usado para relacionar usuario
+  onIds = id => {};
+
   state = {
     userChecked: false,
     userLogged: false
@@ -27,3 +56,7 @@ export default class App extends Component {
     return <Routes />;
   }
 }
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME
+})(App);
